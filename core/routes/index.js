abc46adbc4992cb13e7870d3939b33a87907ba36
samueldurantes <hyossha@gmail.com>
2020-06-27 22:37:54 -0300

    Assigning images.routes for uploads only

--- core/routes/index.js
+++ core/routes/index.js
@@ -3,6 +3,7 @@ const { Router } = require('express')
 const routes = Router()
 
 routes.use('/auth', require('./auth.routes'))
-routes.use('/images', require('./images.routes'))
+routes.use('/upload', require('./upload.routes'))
+routes.use('/user', require('./user.routes'))
 
 module.exports = routes
--- core/routes/upload.routes.js
+++ core/routes/upload.routes.js
@@ -8,18 +8,12 @@ const multerConfig = require('../config/multer')
 const UserSchema = require('../database/schemas/UserSchema')
 const PostSchema = require('../database/schemas/PostSchema')
 
-const imagesRoutes = Router()
+const uploadRoutes = Router()
 
-imagesRoutes.use(authMiddleware)
-imagesRoutes.use('/', static(resolve(__dirname, '..', '..', 'tmp')))
+uploadRoutes.use(authMiddleware)
+uploadRoutes.use('/', static(resolve(__dirname, '..', '..', 'tmp')))
 
-imagesRoutes.get('/', async (req, res) => {
-  const user = await UserSchema.findById(req.userId)
-
-  res.send(user.files)
-})
-
-imagesRoutes.post('/', multer(multerConfig).single('file'), async (req, res) => {
+uploadRoutes.post('/', multer(multerConfig).single('file'), async (req, res) => {
   const { filename: _id, originalname: name, size, location: url = '' } = req.file
 
   const post = await PostSchema.create({ _id, name, size, url, author: req.userId })
@@ -32,10 +26,4 @@ imagesRoutes.post('/', multer(multerConfig).single('file'), async (req, res) => 
   res.json(post)
 })
 
-imagesRoutes.delete('/:id', async (req, res) => {
-  await PostSchema.findByIdAndDelete(req.params.id)
-
-  res.sendStatus(200)
-})
-
-module.exports = imagesRoutes
+module.exports = uploadRoutes
