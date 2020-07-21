f111b636cd75adbd6395315776fa80ff54a948b3
Samuel Durante <hyossha@gmail.com>
2020-07-21 04:43:03 -0300

    Code rework

--- core/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: process.env.AUTH_SECRET,
-  expiresIn: '30d'
-}
--- core/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-
-const routes = Router()
-
-routes.use('/auth', require('./auth'))
-routes.use('/upload', require('./upload'))
-routes.use('/user', require('./user'))
-
-module.exports = routes
